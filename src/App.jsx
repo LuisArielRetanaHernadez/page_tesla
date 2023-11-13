fbdf94debeec71a493b0713cf4f9338ea1daff4d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-13 17:29:35 -0600

    agregando un div para el header y footer y centralos de modo columna

--- src/App.jsx
+++ src/App.jsx
@@ -29,21 +29,28 @@ function App() {
 
         </header>
 
-        <section className="w-screen h-screen text-center text-white bg-black relative overflow-hidden">
+        <section className="w-screen h-screen text-center text-white bg-black flex relative overflow-hidden">
           <div className="h-full w-full absolute top-0 bottom-0">
             <video className="h-full w-full object-center object-cover" autoPlay loop muted src="https://res.cloudinary.com/dqmkovsdy/video/upload/v1699912376/tesla/media/videos/x0x9nksw5hf7ph3aatag.mp4"></video>
           </div>
 
-          <header className="relative z-20">
-            <h2 className="text-5xl font-medium pt-40">Disfruta de Tesla</h2>
-            <p className="text-sm">Programe una prueba de conduccion hoy mismo</p>
-          </header>
+          <div className="h-full w-full flex flex-col relative z-10">
 
-         <footer className=" flex flex-grow flerelative z-20">
-           <a>
-            Prueba de conduccion
-          </a>
-         </footer>
+            <header className="flex flex-col">
+
+              <h2 className="text-5xl font-medium pt-40">Disfruta de Tesla</h2>
+              <p className="text-sm">Programe una prueba de conduccion hoy mismo</p>
+
+            </header>
+
+            <footer className="flex flex-col flex-grow justify-end">
+              <a>
+                Prueba de conduccion
+              </a>
+            </footer>
+
+          </div>
+
          
         </section>
       </mian>
