fbc91bb1ec660294e3a8f9ba2c60ee78f1502d1d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-11-13 23:28:29 -0600

    creando el componente LandingHeader donde contenera el header la page

---
+++ src/components/LandingHeader.jsx
@@ -0,0 +1,34 @@
+import Logo from "./svg/Logo"
+
+const LandingHeader = () => {
+  
+  return (
+    <header className="w-full text-white py-4 px-10 flex items-center justify-between fixed top-0 z-20">
+    <div className="flex flex-grow basis-0">
+      <Logo className="h-6 w-28 "/>
+    </div>
+
+    <nav>
+      <ul className="flex text-sm [&>li>a]:inline-block [&>li>a]:font-medium [&>li>a]:px-4 [&>li>a]:py-2">
+        <li><a href="#">Model S</a></li>
+        <li><a href="#">Model 3</a></li>
+        <li><a href="#">Model X</a></li>
+        <li><a href="#">Model Y</a></li>
+        <li><a href="#">PowerAll</a></li>
+        <li><a href="#">Carga</a></li>
+      </ul>
+    </nav>
+
+    <nav className="flex flex-grow justify-end basis-0">
+      <ul className="flex text-sm [&>li>a]:inline-block [&>li>a]:font-medium [&>li>a]:px-4 [&>li>a]:py-2">
+        <li><a href="#">Soporte</a></li>
+        <li><a href="#">Tienda</a></li>
+        <li><a href="#">Menu</a></li>
+      </ul>     
+    </nav>
+
+  </header>
+  )
+}
+
+export default LandingHeader
